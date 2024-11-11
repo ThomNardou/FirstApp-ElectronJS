310a3968c585de4ca90380e579be9189486ea362
ReactedTitan880 <thomasnardou@icloud.com>
2024-11-11 19:28:58 +0100

    feat(app): try to add push system

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [cloneStatus, setCloneStatus] = useState<string>("");
+    const [pushStatus, setPushStatus] = useState<string>("");
 
     const handleCloneClick = async () => {
         const repoUrl = "https://github.com/ThomNardou/greenteam.git";
@@ -11,12 +12,23 @@ const App = () => {
         setCloneStatus(res);
     };
 
+    const handlePushClick = async () => {
+        const repoPath = `C:/Users/Thomas/Documents/greenteam`;
+        const message = "Test commit";
+        const res = await window.electron.ipcRenderer.invoke('status-add-commit-push', repoPath, message);
+        // const res = await window.Electron?.ipcRenderer.invoke('status-add-commit-push', repoPath, message);
+        setCloneStatus(res);
+    };
+
     return (
         <div>
             <h1>Hello, Electron!</h1>
             <p>I hope you enjoy using basic-electron-react-boilerplate. Test</p>
             <button onClick={handleCloneClick}>Cloner le dépôt</button>
             <p>{cloneStatus}</p>
+
+            <button onClick={handlePushClick}>Pusher le dépôt</button>
+            <p>{pushStatus}</p>
         </div>
     );
 };
